5e5179f845ccda69746465645fc79212f2ff3185
Simon Waller <waller_as@yahoo.co.uk>
2021-03-26 18:01:49 +0000

    first version

---
+++ README.md
@@ -0,0 +1,24 @@
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/volume-mute.svg' card_color='#022B4F' width='50' height='50' style='vertical-align:bottom'/> Pushbutton speak/stop skill
+
+## About
+This Mycroft skill is written assuming that there is a pushbutton connected to a Raspberry Pi. 
+
+A short press of the button is the same as speaking the wake word, allowing the user to then speak a command. A long press is the same as giving the 'stop' command and hence will stop the playback of any audio.
+
+The pin that the button is connected to and the polarity of the signal is configurable via the settings.
+
+## Important
+This skill is made for Picroft Lightning, which is Picroft on Rasbian Stretch.
+
+## Category
+**IoT**
+
+## Credits
+Simon Waller
+
+## Supported Devices
+platform_picroft
+
+## Tags
+media
+tools
---
+++ __init__.py
@@ -0,0 +1,80 @@
+# Copyright Simon Waller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from mycroft import MycroftSkill
+from mycroft.messagebus.message import Message
+from mycroft.util.log import getLogger
+
+import RPi.GPIO as GPIO
+import time
+
+LOGGER = getLogger(__name__)
+
+longpress_threshold = 2
+
+class PushButtonSkill(MycroftSkill):
+
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    def initialize(self):
+        self.settings_change_callback = self.on_settings_changed
+        self.get_settings()
+        if self.button_pin is None:
+            return
+        try:
+            GPIO.setwarnings(False)
+            GPIO.remove_event_detect(self.button_pin)
+            if self.button_polarity == 0:                                      # active low
+                GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+                GPIO.add_event_detect(self.button_pin, GPIO.FALLING)
+            else:                                                              # active high
+                GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+                GPIO.add_event_detect(self.button_pin, GPIO.RISING)
+        except:
+            LOGGER.warning("Can't initialize GPIO - skill will not load")
+            self.speak_dialog("error.initialize")
+            return
+        self.pressed = False
+        self.schedule_repeating_event(self.check_button, None, 0.1, 'ButtonStatus')
+
+    def check_button(self):
+        if self.pressed:
+            if GPIO.input(self.button_pin) != self.button_polarity:
+                self.pressed = False
+                self.pressed_time = time.time() - self.pressed_time
+                if self.pressed_time < longpress_threshold:
+                    self.bus.emit(Message("mycroft.mic.listen"))
+                else:
+                    self.bus.emit(Message("mycroft.stop"))
+        else:
+            if GPIO.event_detected(self.button_pin):
+                self.pressed = True
+                self.pressed_time = time.time()
+                LOGGER.debug("Detected pushbutton interrupt")
+
+    def on_settings_changed(self):
+        self.get_settings()
+        self.led_idle()
+        
+    def get_settings(self):
+        self.button_pin = self.settings.get('button_pin', 24)
+        self.button_polarity = self.settings.get('button_polarity', 0)
+        if (self.button_pin < 0) or (self.button_pin > 27):
+            LOGGER.info("Invalid GPIO pin number")
+            self.button_pin = None
+
+
+def create_skill():
+    return PushButtonSkill()
